829d4d6a6ac23ad97f337e14c6a1f3da9f95f910
GashaPalkin <pavelgalkin.mgn@gmail.com>
2023-12-21 19:06:54 +0500

    убрал некоторые комментарии

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,3 @@
-// * разные способы подключения
-// * ver1 не показывает blade только Vue / мне так удобнее
 import './bootstrap';
 import { createApp } from 'vue';
 import App from '@/App.vue'
@@ -10,11 +8,3 @@ createApp(App)
    .use(Router)
    .use(Store)
    .mount('#app');
-
-// * ver2 показывает еще и blade шаблон если в home.blade указать @extends('layouts.app')
-// import './bootstrap';
-// import { createApp } from 'vue';
-// import App from './src/App.vue'
-// const app = createApp({});
-// app.component('App', App);
-// app.mount('#app');
